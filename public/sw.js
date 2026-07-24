2460f66189fd53a4b26edcb84daaf8d0b922488a
htcptdgroupvn-code <htc.ptdgroup.vn@gmail.com>
2026-07-25 00:41:51 +0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "gungho-sales-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Network-first: luôn ưu tiên lấy bản mới nhất, chỉ dùng cache khi mất mạng
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
